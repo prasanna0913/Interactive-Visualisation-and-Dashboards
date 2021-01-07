e2bcfaab81cb45aa5ba1242b5268c333038e8f7f
prasanna0913 <dspprasanna0913@gmail.com>
2021-01-07 07:29:49 -0500

    pdated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,24 @@
-// Reading and fetching the json data
+// Initialize dashboard on page load
+init();
 
+//  step1 : to create content (dropdown menu items) in dropdown 
 function init() {
     // Read json data
     d3.json('samples.json').then(function (data) {
         console.log(data);
-
         var dropdownoption = d3.select("#selDataset");
         for (var i = 0; i < data.names.length; i++) {
             dropdownoption.append("option").text(data.names[i]);
-
         }
     });
 }
 
+//  step 2: this method triggers only when the users changes the dropdown menu from dropdown 
 function optionChanged(newSample) {
-    d3.selectAll("ul").remove();
     var dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    console.log(dataset);
-    
-
-
-
+    var selectedId = dropdownMenu.property("value");
+    console.log(selectedId);
 }
 
-
-
-
-
-
-
-
-
-// Initialize dashboard on page load
-init();
 
